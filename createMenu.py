c5086429d91878fa2461da5d22f964b39e275923
Karma Tsering Gurung <Karma@karmas-mbp.mnstate.edu>
2016-05-01 14:15:37 -0500

    created test for createMenu, added doc

--- createMenu.py
+++ createMenu.py
@@ -1,5 +1,10 @@
 # Lab2
 def createMenu(optionList):
+  """
+  Description:
+  Pre-condition:
+  post-condition:
+  """
   tmp=" "; ct=0
   for opt in optionList:
     tmp+=str(ct)+" "+opt +"\n"
---
+++ testMenu.py
@@ -0,0 +1,2 @@
+myList=["1. Animal Name","2. American Name", "3. Exit"]
+print(createMenu(myList))
